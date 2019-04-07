76733c902546fb2a7d78fa6efde5f80dd3b1b26b
Joshua <olajidejoshua4real@gmail.com>
2019-04-06 21:02:40 -0700

    Set up the markup and a little design on the index page

---
+++ index.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="bootstrap-4.1.3-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/index.css">
+    <title>HNG | Welcome To Our Blog</title>
+    
+</head>
+<body>
+
+    <header> 
+        <div class="logo"><h1 class="display-4">HNG</h1></div>
+        <div class="other-links">
+            <i class="fa fa-search"></i>
+            <a href="">Become a Member</a>
+            <a href="">Sign in</a>
+            <button class="btn btn-outline-success">Get Started</button>
+        </div>
+    </header>  
+
+    <!-- Navigation starts here -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand display-2 bold" href="#">   </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">ABOUT US</a>
+      </li>
+      
+      <li class="nav-item">
+        <a class="nav-link" href="#" >CONTACT US</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#" >GALLERY</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#" >NEWS</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#" >ENTERTAINMENT</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+    <!-- End of navigation -->
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4">
+           <section>
+
+           </section>
+            </div>
+            <div class="col-lg-4">
+               <section>
+
+               </section>
+            </div>
+            <div class="col-lg-4">
+                <section>
+                    
+                </section>
+            </div>
+            <div class="col-lg-4">
+           <section>
+
+           </section>
+            </div>
+            <div class="col-lg-4">
+               <section>
+
+               </section>
+            </div>
+            <div class="col-lg-4">
+                <section>
+                    
+                </section>
+            </div>
+        </div>
+        <hr>
+        <!-- This section should be for newsletter -->
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="newsletter">
+                </div>
+            </div>
+        </div>
+        <!-- End of newsletter -->
+    </div>
+    <footer>
+        <p class="text-center">&copy copyright HNG 2019 | All Rights Reserved</p>
+    </footer>
+
+<script src="js/jquery-331.min.js"></script>
+<script src="bootstrap-4.1.3-dist/js/bootstrap.min.css"></script>
+<script src="js/popper.min.js"></script>
+</body>
+</html>
